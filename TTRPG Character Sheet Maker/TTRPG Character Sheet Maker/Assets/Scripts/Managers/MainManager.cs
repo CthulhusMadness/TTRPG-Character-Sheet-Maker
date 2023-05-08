eb5ac21c819109e1d6efbb4cea07cf2c03c8f646
Michele Santoni <mikysanto.97@gmail.com>
2023-05-08 23:34:20 +0200

    Changed Initialization() method name to Initialize(); Added CreateCharacter() method; Call SetCharacterInfo() method after creating a new character

--- TTRPG Character Sheet Maker/TTRPG Character Sheet Maker/Assets/Scripts/Managers/MainManager.cs
+++ TTRPG Character Sheet Maker/TTRPG Character Sheet Maker/Assets/Scripts/Managers/MainManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TTRPG.Data;
+using TTRPG.Other;
 using TTRPG.Utility;
 
 namespace TTRPG.Managers
@@ -7,23 +8,31 @@ namespace TTRPG.Managers
     public class MainManager : MonoBehaviour
     {
         #region Fields & Properties
+        [SerializeField] private Context context;
         #endregion Fields & Properties
 
         #region UnityCallbacks
         void Start()
         {
-            Initialization();
+            Initialize();
         }
         #endregion UnityCallbacks
 
         #region Methods
-        private void Initialization()
+        private void Initialize()
+        {
+            var newCharacter = CreateCharacter();
+            context.CurrentCharacterSheet.SetCharacterInfo(newCharacter);
+        }
+
+        private Character CreateCharacter()
         {
             var character = new Character();
             character.player_name = "player";
             character.character_name = "character";
             character.character_level = 1;
             CharacterUtility.SaveJsonFile(character, PathList.CharacterSheetFolder);
+            return character;
         }
         #endregion Methods
     }
