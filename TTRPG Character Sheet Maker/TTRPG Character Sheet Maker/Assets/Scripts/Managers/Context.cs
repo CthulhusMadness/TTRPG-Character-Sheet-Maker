eccd660fa3769cdd6325243300d1bf2839321817
Michele Santoni <mikysanto.97@gmail.com>
2023-05-08 23:32:33 +0200

    Added Context class

---
+++ TTRPG Character Sheet Maker/TTRPG Character Sheet Maker/Assets/Scripts/Managers/Context.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using TTRPG.Managers;
+
+namespace TTRPG.Other
+{
+    public class Context : MonoBehaviour
+    {
+        public CharacterSheetManager CurrentCharacterSheet;
+    }
+}
