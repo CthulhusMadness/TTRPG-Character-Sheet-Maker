1e56135e2b2e73ca75d1eaa5fd33ad41538a0e5c
Michele Santoni <mikysanto.97@gmail.com>
2023-05-10 22:59:55 +0200

    Added CharacterDnD class

---
+++ TTRPG Character Sheet Maker/TTRPG Character Sheet Maker/Assets/Scripts/Data/CharacterDnD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TTRPG.Data
+{
+    [Serializable]
+    public class CharacterDnD : Character
+    {
+        public Race race;
+        public CharacterClass character_class;
+        public AbilityScoresDnD ability_scores;
+        public Feat[] feats;
+    }
+}
