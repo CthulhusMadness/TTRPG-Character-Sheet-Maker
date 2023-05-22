102a230848ab0454b7b6b19a52fbe1baa86712f3
Michele Santoni <mikysanto.97@gmail.com>
2023-05-22 23:40:32 +0200

    Added AbilityScoreField class and DnD variant

---
+++ TTRPG Character Sheet Maker/TTRPG Character Sheet Maker/Assets/Scripts/UI/Fields/AbilityScoreDnDField.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace TTRPG.UI.Fields
+{
+    public class AbilityScoreDnDField : AbilityScoreField
+    {
+        public override int ModifierScore => Mathf.FloorToInt((AbilityScore / 2) - 5);
+
+        protected override void OnEndEdit(string text) => modifierText.SetText(ModifierScore.ToString());
+    }
+}
