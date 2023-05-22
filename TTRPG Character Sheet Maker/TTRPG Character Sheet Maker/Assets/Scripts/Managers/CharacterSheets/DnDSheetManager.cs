de4ad36ec763673eab8c0163577aff4d2708c336
Michele Santoni <mikysanto.97@gmail.com>
2023-05-22 23:39:37 +0200

    Added DnDSheetManager class

---
+++ TTRPG Character Sheet Maker/TTRPG Character Sheet Maker/Assets/Scripts/Managers/CharacterSheets/DnDSheetManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using TMPro;
+using TTRPG.Data;
+using TTRPG.UI;
+
+namespace TTRPG.Managers
+{
+    public class DnDSheetManager : CharacterSheetManager
+    {
+        #region Fields & Properties
+        [SerializeField] protected TMP_InputField classField;
+        [SerializeField] protected TMP_InputField raceField;
+        [SerializeField] protected AbilityScoresDnDManager abilityScores;
+        #endregion Fields & Properties
+
+        #region Methods
+        public override void SetCharacterInfo(Character character)
+        {
+            var currentCharacter = (CharacterDnD)character;
+            base.SetCharacterInfo(currentCharacter);
+            classField.SetTextWithoutNotify(currentCharacter.character_class);
+            raceField.SetTextWithoutNotify(currentCharacter.race);
+            abilityScores.SetAbilityScores(currentCharacter.ability_scores);
+        }
+        #endregion Methods
+    }
+}
