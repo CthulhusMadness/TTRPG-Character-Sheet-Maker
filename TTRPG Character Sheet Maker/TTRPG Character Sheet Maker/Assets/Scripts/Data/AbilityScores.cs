7be3fdc80cf0e4456f44cb924621e6ac66d292c4
Michele Santoni <mikysanto.97@gmail.com>
2023-05-22 23:36:17 +0200

    Added AbilityScoresDnD class

--- TTRPG Character Sheet Maker/TTRPG Character Sheet Maker/Assets/Scripts/Data/AbilityScores.cs
+++ TTRPG Character Sheet Maker/TTRPG Character Sheet Maker/Assets/Scripts/Data/AbilityScores.cs
@@ -6,15 +6,4 @@ namespace TTRPG.Data
     abstract public class AbilityScores
     {
     }
-
-    [Serializable]
-    public class AbilityScoresDnD : AbilityScores
-    {
-        public AbilityScoreDnD strength;
-        public AbilityScoreDnD dexterity;
-        public AbilityScoreDnD constitution;
-        public AbilityScoreDnD intelligence;
-        public AbilityScoreDnD wisdom;
-        public AbilityScoreDnD charisma;
-    }
 }
