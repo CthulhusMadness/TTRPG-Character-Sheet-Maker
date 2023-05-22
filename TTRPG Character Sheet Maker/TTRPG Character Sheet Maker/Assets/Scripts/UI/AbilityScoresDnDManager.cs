47dcb1fb42b24b56a98b46c302a86c7d6025c4fc
Michele Santoni <mikysanto.97@gmail.com>
2023-05-22 23:41:11 +0200

    Added AbilityScoresManager class and DnD variant

---
+++ TTRPG Character Sheet Maker/TTRPG Character Sheet Maker/Assets/Scripts/UI/AbilityScoresDnDManager.cs
@@ -0,0 +1,32 @@
+﻿using TTRPG.Data;
+using TTRPG.UI.Fields;
+using UnityEngine;
+
+namespace TTRPG.UI
+{
+    public class AbilityScoresDnDManager : AbilityScoresManager
+    {
+        #region Fields
+        [SerializeField] protected AbilityScoreDnDField strength;
+        [SerializeField] protected AbilityScoreDnDField dexterity;
+        [SerializeField] protected AbilityScoreDnDField constitution;
+        [SerializeField] protected AbilityScoreDnDField intelligence;
+        [SerializeField] protected AbilityScoreDnDField wisdom;
+        [SerializeField] protected AbilityScoreDnDField charisma;
+
+        #endregion Fields
+
+        #region Methods
+        public override void SetAbilityScores(AbilityScores scores)
+        {
+            AbilityScoresDnD dndScores = (AbilityScoresDnD)scores;
+            strength.SetAbilityScore(dndScores.strength.score);
+            dexterity.SetAbilityScore(dndScores.dexterity.score);
+            constitution.SetAbilityScore(dndScores.constitution.score);
+            intelligence.SetAbilityScore(dndScores.intelligence.score);
+            wisdom.SetAbilityScore(dndScores.wisdom.score);
+            charisma.SetAbilityScore(dndScores.charisma.score);
+        }
+        #endregion Methods
+    }
+}
