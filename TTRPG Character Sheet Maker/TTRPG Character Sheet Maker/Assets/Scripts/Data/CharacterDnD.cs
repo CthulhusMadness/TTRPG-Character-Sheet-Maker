5ad9fb3256e686736664053a4135162316d6fa63
Michele Santoni <mikysanto.97@gmail.com>
2023-05-22 23:37:15 +0200

    Added constructors to Character and CharacterDnD classes

--- TTRPG Character Sheet Maker/TTRPG Character Sheet Maker/Assets/Scripts/Data/CharacterDnD.cs
+++ TTRPG Character Sheet Maker/TTRPG Character Sheet Maker/Assets/Scripts/Data/CharacterDnD.cs
@@ -9,5 +9,15 @@ namespace TTRPG.Data
         public string character_class;
         public AbilityScoresDnD ability_scores;
         public Feat[] feats;
+
+        public CharacterDnD() { }
+        public CharacterDnD(string playerName, string characterName, int characterLevel, string race, string character_class, AbilityScoresDnD ability_scores, Feat[] feats)
+            : base(playerName, characterName, characterLevel)
+        {
+            this.race = race;
+            this.character_class = character_class;
+            this.ability_scores = ability_scores;
+            this.feats = feats;
+        }
     }
 }
