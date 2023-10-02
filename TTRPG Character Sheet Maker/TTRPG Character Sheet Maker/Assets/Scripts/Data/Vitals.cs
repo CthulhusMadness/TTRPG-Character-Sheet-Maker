ed27900a6c4897aefb16180e77d002c297815c69
Michele Santoni <mikysanto.97@gmail.com>
2023-10-02 22:39:27 +0200

    Added Vitals class and DnD variation

---
+++ TTRPG Character Sheet Maker/TTRPG Character Sheet Maker/Assets/Scripts/Data/Vitals.cs
@@ -0,0 +1,14 @@
+﻿namespace TTRPG.Data
+{
+    public class Vitals
+    {
+        public int current_hp;
+        public int max_hp;
+
+        public Vitals(int currentHp, int maxHp)
+        {
+            current_hp = currentHp;
+            max_hp = maxHp;
+        }
+    }
+}
